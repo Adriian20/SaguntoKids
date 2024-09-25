0fa1077709ddeb91a163fdbf8b5950b1642379be
Adriian20 <adrianmarquesroqueta@gmail.com>
2024-09-25 19:33:56 +0200

    Config

--- src/main/java/com/example/saguntokids/config.java
+++ src/main/java/com/example/saguntokids/config.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class config implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
